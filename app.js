ca01473df59a1aad5020196ea61529e314608804
Gabriele Stefani Teixeira <87157629+gbrielest@users.noreply.github.com>
2021-10-05 20:58:42 -0300

    Atualização do laço de repetição JS

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 function CarregarFilmes(){
     //array --> variável com subdivisões (índices)
-    let filmes = ["images/guerra.jpg", "images/viuva.jpg", "images/pantera.jpg", "images/capita.jpg" , "images/guardioes.jpg" , "images/formiga.jpg" , "images/spider.jpg" , "images/thor.jpg"]
+    let filmes = [
+        "images/guerra.jpg" , 
+        "images/viuva.jpg" , 
+        "images/pantera.jpg" , 
+        "images/capita.jpg" , 
+        "images/guardioes.jpg" , 
+        "images/formiga.jpg" , 
+        "images/spider.jpg" , 
+        "images/thor.jpg"
+    ]
+
+    let totalFilmes = filmes.length
     
     //laço de repetição
-    for(let controle = 0; controle < 8 ; controle = controle + 1){
-        document.querySelector(".lista-filmes").innerHTML += "<img src="+ filmes [controle] + ">"
+    for(let i = 0 ; i < totalFilmes ; i++){
+        document.querySelector(".lista-filmes").innerHTML += "<img src="+ filmes [i] + ">"
 
     }
 }
