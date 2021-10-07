cc696930562355be5d8517afd3bfd6bd6af8047e
Gabriele Stefani Teixeira <87157629+gbrielest@users.noreply.github.com>
2021-10-06 21:47:58 -0300

    Mudaças na Visualização

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-function CarregarFilmes(){
-    //array --> variável com subdivisões (índices)
-    let filmes = [
-        "images/guerra.jpg" , 
-        "images/viuva.jpg" , 
-        "images/pantera.jpg" , 
-        "images/capita.jpg" , 
-        "images/guardioes.jpg" , 
-        "images/formiga.jpg" , 
-        "images/spider.jpg" , 
-        "images/thor.jpg"
-    ]
+//array --> variável com subdivisões (índices)
+let filmes = [
+    "images/guerra.jpg" , 
+    "images/viuva.jpg" , 
+    "images/pantera.jpg" , 
+    "images/capita.jpg" , 
+    "images/guardioes.jpg" , 
+    "images/formiga.jpg" , 
+    "images/spider.jpg"
+]
 
+//refatoração
+//melhorar um código existente.
+
+function CarregarFilmes(){
     let totalFilmes = filmes.length
+    let divListaFilmes = document.querySelector(".lista-filmes")
+
+    divListaFilmes.innerHTML = ""
     
     //laço de repetição
     for(let i = 0 ; i < totalFilmes ; i++){
         document.querySelector(".lista-filmes").innerHTML += "<img src="+ filmes [i] + ">"
 
     }
+}
+
+function AdicionarFilme(){
+    let nomeFilme = document.querySelector("#nome-do-filme").value
+    let caminhoCompleto = "images/" + nomeFilme
+
+    filmes.push(caminhoCompleto)
+
+    CarregarFilmes()
 }
